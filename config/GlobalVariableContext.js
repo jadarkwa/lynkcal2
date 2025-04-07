d9ed48b5f4167cf4e111137e123d57b3657041d6
Jude Adarkwa <jadarkwa@umich.edu>
2025-04-07 14:39:19 -0400

    dev comit

--- config/GlobalVariableContext.js
+++ config/GlobalVariableContext.js
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const DeviceVariables = {
   'Instagram Icon': 'Icon',
-  __env__: 'Production',
+  __env__: 'Development',
 };
 export const AppVariables = {};
 const GlobalVariableContext = React.createContext();
